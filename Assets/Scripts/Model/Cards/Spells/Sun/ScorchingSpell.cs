6e7eba5a97cd7ccf8819349762f76969eb31829c
nyarla <nyarladev@gmail.com>
2022-06-09 11:09:55 +0300

    Scorching reworked

--- Assets/Scripts/Model/Cards/Spells/Sun/ScorchingSpell.cs
+++ Assets/Scripts/Model/Cards/Spells/Sun/ScorchingSpell.cs
@@ -1,5 +1,4 @@
-﻿using Model.Cards.Combat;
-using Model.Combat.Characters;
+﻿using Model.Combat.Characters;
 using Model.Combat.Effects;
 using Model.Localization;
 
@@ -12,25 +11,18 @@ namespace Model.Cards.Spells.Sun
             "Опаление"
         );
         public override LocalizedString Description => new LocalizedString(
-            "Deal 2<dm>. You may discard a <cr> to deal 3<dm> more",
-            "Наносит 2<dm>. Вы можете сбросить <cr>, чтобы нанести на 3<dm> больше."
+            "Deal 2<dm>. Gain 1<br>",
+            "Наносит 2<dm>. Получите 1<br>"
         );
         public override SpellType Type => SpellType.None;
         public override async void OnPlay(bool burst)
         {
-            int damage = 2;
-            CardInHand cardToDiscard = await GetTarget<CardInHand>(ChooseCardToDiscard, false);
-            if (cardToDiscard != null)
-            {
-                GameBoard.EffectQueue.AddEffect(new DiscardACardEffect(0.1f, cardToDiscard));
-                damage += 3;
-            }
-            Enemy enemy = await GetTarget<Enemy>(ChooseEnemyMessage, true);
-            if (enemy != null)
-            {
-                GameBoard.EffectQueue.AddEffect(new DealDamageEffect(0.1f, enemy, damage, burst));
-            }
             base.OnPlay(burst);
+            Enemy enemy = await GetTarget<Enemy>(ChooseEnemyMessage, true);
+            if (enemy == null)
+                return;
+            GameBoard.EffectQueue.AddEffect(new DealDamageEffect(0.1f, enemy, 2, burst));
+            GameBoard.EffectQueue.AddEffect(new AddBurstEffect(0.1f, GameBoard.PlayerBoard, 1));
         }
     }
 }
